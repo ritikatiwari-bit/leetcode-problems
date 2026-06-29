7456d310ed8ecd9a44a4e507f5426e8ed9622435
Ritika Tiwari <ritika.tiwari.7612@gmail.com>
2026-06-29 21:56:20 +0530

    Time: 27 ms (99.32%), Space: 53.4 MB (62.12%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -2,10 +2,7 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    return args.length;
-    
-};
+var argumentsLength = (...args) => args.length;
 
 /**
  * argumentsLength(1, 2, 3); // 3
