9901ea9be4e2390bb4c116f732a9d0e78c0c3bb6
Ritika Tiwari <ritika.tiwari.7612@gmail.com>
2026-08-07 12:15:17 +0530

    Time: 2 ms (67.32%), Space: 85.1 MB (81.03%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -5,8 +5,6 @@ class Solution {
             if(nums[i]>nums[i+1]){
                 inc = false;
             }
-        }
-        for(int i=0;i<nums.length-1;i++){
             if(nums[i]<nums[i+1]){
                 dec = false;
             }
